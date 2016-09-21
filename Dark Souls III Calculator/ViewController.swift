7d8c88249698baf9973f9a5f1d9ddd62fc46df07
Donald Lowery <donaldlowery@Donalds-Mac.local>
2016-09-21 15:43:33 -0700

    Initial Commit

---
+++ Dark Souls III Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dark Souls III Calculator
+//
+//  Created by Donald Lowery on 9/21/16.
+//  Copyright © 2016 Donald Lowery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
